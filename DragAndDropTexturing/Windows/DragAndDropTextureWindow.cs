f32e014ff61e020b3c3925972a9c3c4db2a1900e
Sebastina <sebane1@thavea.com>
2026-06-01 21:00:47 -0500

    Tighten sanity check

--- DragAndDropTexturing/Windows/DragAndDropTextureWindow.cs
+++ DragAndDropTexturing/Windows/DragAndDropTextureWindow.cs
@@ -234,27 +234,25 @@ namespace RoleplayingVoice
                     string overlayKey = !string.IsNullOrEmpty(diffusePath) ? diffusePath : (!string.IsNullOrEmpty(normalPath) ? normalPath : maskPath);
                     System.Numerics.Vector4 tintColor = System.Numerics.Vector4.One;
 
-                    // This variable does nothing. Sanity check to ensure github is using up to date code.
-                    int sanityCheck = 0;
-
-                    if (!_collectionSortedPenumbraOverlayTints.ContainsKey(collectionId))
-                    {
-                        _collectionSortedPenumbraOverlayTints.Add(collectionId, new Dictionary<string, Vector4>());
-                    }
-                    if (!_collectionSortedPenumbraOverlayGlowTints.ContainsKey(collectionId))
-                    {
-                        _collectionSortedPenumbraOverlayGlowTints.Add(collectionId, new Dictionary<string, Vector4>());
-                    }
-                    if (!_collectionSortedPenumbraOverlayTints[collectionId].ContainsKey(overlayKey))
-                    {
-                        _collectionSortedPenumbraOverlayTints[collectionId].Add(overlayKey, new Vector4());
-                    }
-                    if (!_collectionSortedPenumbraOverlayGlowTints[collectionId].ContainsKey(overlayKey))
-                    {
-                        _collectionSortedPenumbraOverlayGlowTints[collectionId].Add(overlayKey, new Vector4());
-                    }
                     try
                     {
+                        if (!_collectionSortedPenumbraOverlayTints.ContainsKey(collectionId))
+                        {
+                            _collectionSortedPenumbraOverlayTints.Add(collectionId, new Dictionary<string, Vector4>());
+                        }
+                        if (!_collectionSortedPenumbraOverlayGlowTints.ContainsKey(collectionId))
+                        {
+                            _collectionSortedPenumbraOverlayGlowTints.Add(collectionId, new Dictionary<string, Vector4>());
+                        }
+                        if (!_collectionSortedPenumbraOverlayTints[collectionId].ContainsKey(overlayKey))
+                        {
+                            _collectionSortedPenumbraOverlayTints[collectionId].Add(overlayKey, new Vector4());
+                        }
+                        if (!_collectionSortedPenumbraOverlayGlowTints[collectionId].ContainsKey(overlayKey))
+                        {
+                            _collectionSortedPenumbraOverlayGlowTints[collectionId].Add(overlayKey, new Vector4());
+                        }
+
                         if (overlayKey != null && _collectionSortedPenumbraOverlayTints[collectionId].TryGetValue(overlayKey, out var savedTint))
                         {
                             tintColor = savedTint;
